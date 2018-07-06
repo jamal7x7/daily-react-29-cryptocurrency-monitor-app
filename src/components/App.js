6a75a1e47b157c873a90ee8c54f0061544b39370
jamal7X7 <jamal7x7@gmail.com>
2018-07-06 22:54:57 +0100

    svg

--- src/components/App.js
+++ src/components/App.js
@@ -18,12 +18,16 @@ const Items = (props) => (
       <div className="time">ALL</div>
     </div>
     <div className='graph'>
-      Graph
+
+      <svg height= '300' width= '700'>
+        <polygon points={'50,300 300,' + Number(props.fractionPrice) + ' 650,300'} />
+      </svg>
+      
     </div>
     <div className="foot">
       <div className="price">
         <div className="price-main"> {props.currentPrice} </div>
-        <div className="price-fraction">.48</div>
+        <div className="price-fraction">.{props.fractionPrice} </div>
         <div className="currency">USD</div>
         <div className="delta">+ 101.21 (1.7%)</div>
       </div>
@@ -44,7 +48,32 @@ const Menu = () => (
 class CryptoMonitorApp extends Component {
   
   state = {
-    currentPrice: 6677
+    
+    currentPrice: 6677,
+    points: []
+  }
+
+  updatePrice = (data) => {
+    this.setState((prevState) => ({
+      currentPrice: Math.trunc(data.BTC.USD),
+      fractionPrice: (data.BTC.USD).toFixed(2).split('.')[1]
+    }))
+    
+    console.log(this.state.fractionPrice)
+    
+  }
+  
+  addPoint = (data) => {
+    this.setState((prevState) => ({
+      points: [...prevState.points,(data.BTC.USD)]
+    }))
+    
+    this.state.points.length > 10 && 
+      this.setState((prevState) => ({
+        points: []
+      }))
+    console.log(this.state.points)
+    
   }
 
   componentDidMount() {
@@ -53,22 +82,18 @@ class CryptoMonitorApp extends Component {
     const url = 'https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH&tsyms=USD' 
     // const url = 'https://api.coinbase.com/v2/prices/spot?currency=' + query 
     // const url = 'https://api.binance.com/api/v1/ticker/24hr'
+    // const url = 'https://api.coindesk.com/v1/bpi/historical/close.json?start=2013-09-01&end=2018-07-01'
     
     console.log(url)
     
     setInterval ( () => {
-      
-       fetch(url)
-          .then(res =>  res.json())
-          .then(data => {
-            console.log(data.BTC.USD)
-  
-            this.setState((prevState) => ({
-              currentPrice: Math.floor(data.BTC.USD)
-            }))
-            
-          })
-          .catch(error => { console.log('Something went wrong!!!', error) })
+      fetch(url)
+      .then(res =>  res.json())
+      .then(data => {
+        this.addPoint(data) 
+        this.updatePrice(data)
+      })
+      .catch(error => { console.log('Something went wrong!!!', error) })
 
      } ,1000)
   }
@@ -83,6 +108,7 @@ class CryptoMonitorApp extends Component {
         <Menu />
         <Items 
           currentPrice={this.state.currentPrice}
+          fractionPrice={this.state.fractionPrice}
         />
 
       </div>
